3621415cbdde1c1ef70f9b233ff6046d60fbb741
H1Do <90624145+H1Do@users.noreply.github.com>
2023-04-23 22:08:42 +0500

    Добавлены комментарии к коду "methods.cpp"

--- methods.cpp
+++ methods.cpp
@@ -2,40 +2,40 @@
 
 class A {
 public:
-  A() { std::cout << "A()" << std::endl; }
-  virtual ~A() { std::cout << "~A()" << std::endl; }
-  void casualMethod() { std::cout << "A::casualMethod()" << std::endl; }
-  virtual void virtualMethod() { std::cout << "A::virtualMethod" << std::endl; }
+  A() { std::cout << "A()" << std::endl; } // Конструктор
+  virtual ~A() { std::cout << "~A()" << std::endl; } // Виртуальный деструктор
+  void casualMethod() { std::cout << "A::casualMethod()" << std::endl; } // Простой метод
+  virtual void virtualMethod() { std::cout << "A::virtualMethod" << std::endl; } // Виртуальный метод
 };
 
 class B : public A {
 public:
-  B() { std::cout << "B()" << std::endl; }
-  ~B() { std::cout << "~B()" << std::endl; }
-  void casualMethod() { std::cout << "B::casualMethod()" << std::endl; }
-  void virtualMethod() override { std::cout << "B::virtualMethod" << std::endl; }
+  B() { std::cout << "B()" << std::endl; } // Конструктор
+  ~B() { std::cout << "~B()" << std::endl; } // Всё еще виртуальный деструктор
+  void casualMethod() { std::cout << "B::casualMethod()" << std::endl; } // Перекрытый метод
+  void virtualMethod() override { std::cout << "B::virtualMethod" << std::endl; } // Переопределенный метод
 };
 
 int main() {
-  std::cout << "Constructors:" << std::endl;
-  A* first_object = new A();
-  A* second_object = new B();
-  B* third_object = new B();
+  std::cout << "Constructors:" << std::endl; // Вызываем конструкторы классов 
+  A* first_object = new A(); // A()
+  A* second_object = new B(); // A() B()
+  B* third_object = new B(); // A() B()
 
-  std::cout << "Common methods:" << std::endl;
-  first_object->casualMethod();
-  second_object->casualMethod();
-  third_object->casualMethod();
+  std::cout << "Common methods:" << std::endl; // Вызываем перекрываемые методы
+  first_object->casualMethod(); // A::casualMethod()
+  second_object->casualMethod(); // A::casualMethod()
+  third_object->casualMethod(); // B::casualMethod()
 
-  std::cout << "Virtual methods:" << std::endl;
-  first_object->virtualMethod();
-  second_object->virtualMethod();
-  third_object->virtualMethod();
+  std::cout << "Virtual methods:" << std::endl; // Вызываем переопределяемые методы
+  first_object->virtualMethod(); // A::virtualMethod()
+  second_object->virtualMethod(); // B::virtualMethod()
+  third_object->virtualMethod(); // B::virtualMethod()
     
-  std::cout << "Destructors:" << std::endl;
-  delete first_object;
-  delete second_object;
-  delete third_object;
+  std::cout << "Destructors:" << std::endl; // Вызываем деструкторы
+  delete first_object; // ~A()
+  delete second_object; // ~B() ~A()
+  delete third_object; // ~B() ~A()
 
   return 0;
 }
