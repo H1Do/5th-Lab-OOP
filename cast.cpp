f8effc35cd67e1e6bc97130c881ed0ac5cb3dad9
H1Do <90624145+H1Do@users.noreply.github.com>
2023-04-24 14:25:36 +0500

    Добавлены комментарии к коду "cast.cpp"

--- cast.cpp
+++ cast.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
 
-class Essence {
+// Класс сущности (живой или не живой)
+class Essence { 
 public:
   Essence() { std::cout << "Essence()" << std::endl; }
   ~Essence() { std::cout << "~Essence()" << std::endl; }
@@ -9,15 +10,17 @@ public:
   virtual bool isA(std::string something) { return something == "Essence"; }
 };
 
+// Класс существа (живая сущность)
 class Creature : public Essence {
 public:
   Creature() { std::cout << "Creature()" << std::endl; }
   ~Creature() { std::cout << "~Creature()" << std::endl; }
   std::string classname() { return "Creature"; }
-  bool isA(std::string something) { return something == "Creature"; }
+  bool isA(std::string something) { return something == "Creature" || Essence::isA(something); }
   void breath() { std::cout << "The creature is breathing" << std::endl; }
 };
 
+// Класс человека (человеческая живая сущность)
 class Human : public Creature {
 public:
   Human() { std::cout << "Human()" << std::endl; }
@@ -26,6 +29,7 @@ public:
   bool isA(std::string something) { return something == "Human" || Creature::isA(something); }
 };
 
+// Класс вещи (не живая сущность)
 class Thing : public Essence {
 public:
   Thing() { std::cout << "Thing()" << std::endl; }
@@ -39,28 +43,31 @@ int main() {
 
   container[0] = new Creature();
   container[3] = new Creature();
-  container[6] = new Creature();
+  container[6] = new Creature(); std::cout << std::endl;
   container[1] = new Human();
   container[4] = new Human();
-  container[7] = new Human();
+  container[7] = new Human(); std::cout << std::endl;
   container[2] = new Thing();
   container[5] = new Thing();
   container[8] = new Thing();
 
-  std::cout << "Unsafe casting:" << std::endl;
-  std::cout << "classname():" << std::endl;
+  std::cout << std::endl;
+  std::cout << "Unsafe casting:" << std::endl; // Явное приведение типов через указатель на базовый класс - Небезопасно
+  std::cout << "classname():" << std::endl; // Проверка на принадлежность с помощью метода classname()
 
   for (auto i : container)
-    if (i->classname() == "Creature" || i->classname() == "Human")
+    if (i->classname() == "Creature")
       ((Creature*)i)->breath(); 
 
-  std::cout << "isA(std::string something):" << std::endl;
+  std::cout << std::endl;
+  std::cout << "isA(std::string something):" << std::endl; // Проверка на принадлежность с помощью метода isA()
 
   for (auto i : container)
     if (i->isA("Creature"))
       ((Creature*)i)->breath(); 
 
-  std::cout << "Safe casting:" << std::endl;
+  std::cout << std::endl;
+  std::cout << "Safe casting:" << std::endl; // Приведение через dynamic_cast с проверкой на корректность приведения - Безопасно
 
   for (auto i : container)
     if (dynamic_cast<Creature*>(i) != NULL)
